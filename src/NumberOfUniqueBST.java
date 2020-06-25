1077c2dba5ad32178692249e07263ef9573eb519
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2020-06-25 08:50:31 -0400

    Number of unique BST

---
+++ src/NumberOfUniqueBST.java
@@ -0,0 +1,13 @@
+
+public class NumberOfUniqueBST {
+	  public int numTrees(int n) {
+		  
+		  //catalan number - O(n) time complexity
+		    // Note: we should use long here instead of int, otherwise overflow
+		    long C = 1;
+		    for (int i = 0; i < n; ++i) {
+		      C = C * 2 * (2 * i + 1) / (i + 2);
+		    }
+		    return (int) C;
+		  }
+	}
